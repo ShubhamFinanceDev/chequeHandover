42238e7af381fb9560d7c8c476b73bd26bca4ea0
Atishra <atish.rai@dbalounge.com>
2024-05-13 16:15:38 +0530

    Create user api

--- src/main/java/cheque/handover/services/Controller/Admin.java
+++ src/main/java/cheque/handover/services/Controller/Admin.java
@@ -1,13 +1,32 @@
 package cheque.handover.services.Controller;
 
+import cheque.handover.services.Entity.UserDetail;
+import cheque.handover.services.Model.CommonResponse;
+import cheque.handover.services.Services.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/admin")
 public class Admin {
+    @Autowired
+    private Service service;
 
-
+    @PostMapping("/createUser")
+    public CommonResponse userdetail(@RequestBody UserDetail userDetail) {
+        CommonResponse commonResponse = new CommonResponse();
+        String emailId = userDetail.getEmailId();
+        String password = userDetail.getPassword();
+        if (emailId != null && emailId.contains("@shubham") && userDetail.getPassword() != null) {
+            commonResponse = service.saveuser(userDetail);
+        } else {
+            commonResponse.setCode("1111");
+            commonResponse.setMsg("invalid user-email");
+        }
+        return commonResponse;
+    }
 }
--- src/main/java/cheque/handover/services/ServiceImpl/ServiceImpl.java
+++ src/main/java/cheque/handover/services/ServiceImpl/ServiceImpl.java
@@ -1,7 +1,9 @@
 package cheque.handover.services.ServiceImpl;
 
 import cheque.handover.services.Controller.User;
+import cheque.handover.services.Entity.AssignBranch;
 import cheque.handover.services.Entity.BranchMaster;
+import cheque.handover.services.Entity.RoleMaster;
 import cheque.handover.services.Entity.UserDetail;
 import cheque.handover.services.Model.BranchesResponse;
 import cheque.handover.services.Model.CommonResponse;
@@ -12,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -24,6 +27,7 @@ public class ServiceImpl implements cheque.handover.services.Services.Service {
     private UserDetailRepo userDetailRepo;
     @Autowired
     private BranchMasterRepo branchMasterRepo;
+    BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
     private final Logger logger = LoggerFactory.getLogger(User.class);
 
     public ResponseEntity<?> findUserDetails(String emailId) {
@@ -87,5 +91,43 @@ public class ServiceImpl implements cheque.handover.services.Services.Service {
         branchesResponse.setCommanResponse(commonResponse);
         branchesResponse.setBranchMasters(branchByName);
 
+    }
+    @Override
+    public CommonResponse saveuser(UserDetail userDetail) {
+        CommonResponse commonResponse = new CommonResponse();
+        UserDetail userDetails=new UserDetail();
+        RoleMaster userRoleDetail=new RoleMaster();
+        List<AssignBranch> assignBranchList = new ArrayList<>();
+        try {
+            Optional<UserDetail> emailExist = userDetailRepo.findUser(userDetail.getEmailId());
+            if (emailExist.isEmpty()) {
+                userDetails.setPassword(passwordEncoder.encode(userDetail.getPassword()));
+                userDetails.setEmailId(userDetail.getEmailId());
+                userDetails.setPassword(userDetail.getPassword());
+                userDetails.setFirstname(userDetail.getFirstname());
+                userDetails.setLastName(userDetail.getLastName());
+                userDetails.setMobileNo(userDetail.getMobileNo());
+
+                userRoleDetail.setRole(String.valueOf(userDetail.getRoleMasters().getRole()));
+                userRoleDetail.setUserMaster(userDetails);
+                userDetails.setRoleMasters(userRoleDetail);
+
+                for (AssignBranch branch : userDetail.getAssignBranches()) {
+                    branch.setUserMaster(userDetails);
+                    assignBranchList.add(branch);
+                }
+                userDetails.setAssignBranches(assignBranchList);
+                userDetailRepo.save(userDetails);
+                commonResponse.setCode("0000");
+                commonResponse.setMsg("User saved successfully");
+            } else {
+                commonResponse.setCode("1111");
+                commonResponse.setMsg("User already exists");
+            }
+        } catch (Exception e) {
+            commonResponse.setCode("1111");
+            commonResponse.setMsg("Error: " + e.getMessage());
+        }
+        return commonResponse;
     }
 }
