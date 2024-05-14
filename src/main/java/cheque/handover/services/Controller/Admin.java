75154e3d267305f76fc068694531b18f567cce06
Atishra <atish.rai@dbalounge.com>
2024-05-14 15:19:26 +0530

    add response entity

--- src/main/java/cheque/handover/services/Controller/Admin.java
+++ src/main/java/cheque/handover/services/Controller/Admin.java
@@ -4,6 +4,8 @@ import cheque.handover.services.Entity.UserDetail;
 import cheque.handover.services.Model.CommonResponse;
 import cheque.handover.services.Services.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,17 +15,18 @@ public class Admin {
     @Autowired
     private Service service;
 
-    @PostMapping("/createUser")
-    public CommonResponse userdetail(@RequestBody UserDetail userDetail/*@RequestParam(value = "created_By",required = false)String createdBy*/) {
+    @PostMapping("/create-user")
+    public ResponseEntity<CommonResponse> userdetail(@RequestBody UserDetail userDetail) {
         CommonResponse commonResponse = new CommonResponse();
         String emailId = userDetail.getEmailId();
         String password = userDetail.getPassword();
         if (emailId != null && emailId.contains("@shubham") && userDetail.getPassword() != null) {
             commonResponse = service.saveuser(userDetail);
+            return ResponseEntity.ok(commonResponse);
         } else {
             commonResponse.setCode("1111");
             commonResponse.setMsg("invalid user-email");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(commonResponse);
         }
-        return commonResponse;
     }
 }
