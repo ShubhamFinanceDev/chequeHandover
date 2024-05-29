ff394aa0a83402386cd8f61ac39211586224311c
Atishra <atish.rai@dbalounge.com>
2024-05-29 17:23:44 +0530

    add logic for import excel or branch master

--- src/main/java/cheque/handover/services/Repository/BranchMasterRepo.java
+++ src/main/java/cheque/handover/services/Repository/BranchMasterRepo.java
@@ -31,4 +31,8 @@ public interface BranchMasterRepo extends JpaRepository<BranchMaster,Long> {
 
     @Query("select b.branchName from BranchMaster b where b.branchCode in :branchCode")
     List<String> findByBranchCode(List<Long> branchCode);
+
+    boolean existsByBranchCode(String branchCode);
+
+
 }
