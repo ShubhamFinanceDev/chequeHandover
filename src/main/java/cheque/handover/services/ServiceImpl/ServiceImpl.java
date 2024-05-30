868bf616e6ec78dbbeaff304699971f56de736da
nainish <pmo_dbalounge@shubham.co>
2024-05-30 17:16:37 +0530

    mask phone no

--- src/main/java/cheque/handover/services/ServiceImpl/ServiceImpl.java
+++ src/main/java/cheque/handover/services/ServiceImpl/ServiceImpl.java
@@ -118,7 +118,7 @@ public class ServiceImpl implements cheque.handover.services.Services.Service {
             userDetails.setFirstname(userData.getFirstname());
             userDetails.setLastName(userData.getLastName());
             userDetails.setEmailId(userData.getEmailId());
-            userDetails.setMobileNo(userData.getMobileNo());
+            userDetails.setMobileNo("******"+userData.getMobileNo().substring(userData.getMobileNo().length()-4,userData.getMobileNo().length()));
             userDetails.setCreatedBy(userData.getCreatedBy());
             userDetails.setEnabled(userData.isEnabled());
             userDetails.setCreateDate(String.valueOf(userData.getCreateDate()));
