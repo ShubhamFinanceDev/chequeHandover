4d7df3cfde17940730ef4caf6c6b8106145027a3
nainish <pmo_dbalounge@shubham.co>
2024-06-16 18:19:24 +0530

    ddfs path

--- src/main/java/cheque/handover/services/Utility/DdfsUtility.java
+++ src/main/java/cheque/handover/services/Utility/DdfsUtility.java
@@ -94,7 +94,7 @@ public class DdfsUtility {
             formData.add("clientUserId", "06799");
             formData.add("remarks", "");
             formData.add("maker", "06799");
-            formData.add("path", "REKYC");
+            formData.add("path", "Cheque Handover");
             formData.add("document", base64);
 
             HttpHeaders headers = new HttpHeaders();
