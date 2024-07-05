f5d215c46bf769178b1ec8475a11ea9a563886d1
nainish <pmo_dbalounge@shubham.co>
2024-07-05 14:56:28 +0530

    uat changes of user screen, all branch

--- src/main/java/cheque/handover/services/Utility/MisReportUtility.java
+++ src/main/java/cheque/handover/services/Utility/MisReportUtility.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Data
 @Service
 public class MisReportUtility {
-        public String misQuery(String reportType, String selectedType, String fromDate, String toDate, String selectedDate, String status) {
+    public String misQuery(String reportType, String selectedType, String fromDate, String toDate, String selectedDate, String status) {
         StringBuilder baseQuery = new StringBuilder("SELECT em.applicant_name, em.loan_amount, em.cheque_amount, em.branch_name,em.application_number, cs.consumer_type, cs.handover_date, cs.updated_by FROM import_data em JOIN issued_cheque cs ON em.cheque_id = cs.cheque_id \n");
         switch (reportType.toLowerCase()) {
             case "user-wise":
@@ -14,49 +14,24 @@ public class MisReportUtility {
                 if (selectedType != null && !selectedType.isEmpty()) {
                     baseQuery.append("AND cs.updated_by = '").append(selectedType).append("' ");
                 }
-                if (!status.isEmpty()) {
-                    if (status.equalsIgnoreCase("issued")) {
-                        baseQuery.append("AND em.cheque_status = 'Y' ");
-                    } else if (status.equalsIgnoreCase("not-issued")) {
-                        baseQuery = new StringBuilder("SELECT em.applicant_name, em.loan_amount, em.cheque_amount, em.branch_name,em.application_number, cs.consumer_type, cs.handover_date, cs.updated_by FROM import_data em JOIN issued_cheque cs ON em.cheque_id = cs.cheque_id  WHERE em.cheque_status = 'N' AND cs.updated_by = '").append(selectedType).append("' ");
-                    }
+                if (status.equalsIgnoreCase("not-issued")) {
+                    baseQuery = new StringBuilder("SELECT em.applicant_name, em.loan_amount, em.cheque_amount, em.branch_name, em.application_number FROM import_data em WHERE em.cheque_status = 'N' AND em.uploaded_by = '").append(selectedType).append("' ");
                 }
-                if (!fromDate.isEmpty() && !toDate.isEmpty()) {
-                    baseQuery.append("AND DATE(cs.updated_date) BETWEEN '").append(fromDate).append("' AND '").append(toDate).append("' ");
-                }
-                if (!selectedDate.isEmpty()) {
-                    baseQuery.append("AND DATE(cs.updated_date) = '").append(selectedDate).append("' ");
-                }
+                fromAndToSelectedDate(status, fromDate, toDate, selectedDate, baseQuery);
                 break;
 
             case "branch-wise":
 
                 if (selectedType != null && !selectedType.isEmpty()) {
                     baseQuery.append("AND em.branch_name = '").append(selectedType).append("' ");
+                } else if (status.equalsIgnoreCase("not-issued")) {
+                    baseQuery = new StringBuilder("SELECT em.applicant_name, em.loan_amount, em.cheque_amount, em.branch_name, em.application_number FROM import_data em WHERE em.cheque_status = 'N' AND em.branch_name= '").append(selectedType).append("' ");
                 }
-                if (!status.isEmpty()) {
-                    if (status.equalsIgnoreCase("issued")) {
-                        baseQuery.append("AND em.cheque_status = 'Y' ");
-                    } else if (status.equalsIgnoreCase("not-issued")) {
-                        baseQuery = new StringBuilder("SELECT em.applicant_name, em.loan_amount, em.cheque_amount, em.branch_name, em.application_number FROM import_data em WHERE em.cheque_status = 'N' AND em.branch_name= '").append(selectedType).append("' ");
-                    }
-                }
-                if (!fromDate.isEmpty() && !toDate.isEmpty() && !status.equalsIgnoreCase("not-issued")) {
-                    baseQuery.append("AND DATE(cs.updated_date) BETWEEN '").append(fromDate).append("' AND '").append(toDate).append("' ");
-                }
-                if (!selectedDate.isEmpty() && !status.equalsIgnoreCase("not-issued")) {
-                    baseQuery.append("AND DATE(cs.updated_date) = '").append(selectedDate).append("' ");
-                }
+                fromAndToSelectedDate(status, fromDate, toDate, selectedDate, baseQuery);
                 break;
 
             case "daily-report":
-
-                baseQuery.append("WHERE em.cheque_status = 'Y' ");
-                if (!selectedDate.isEmpty()) {
-                    baseQuery.append("AND DATE(cs.updated_date) = '").append(selectedDate).append("' ");
-                } else {
-                    throw new IllegalArgumentException("For daily-report, selectedDate must be provided.");
-                }
+                fromAndToSelectedDate(status, fromDate, toDate, selectedDate, baseQuery);
                 break;
 
             default:
@@ -66,4 +41,21 @@ public class MisReportUtility {
         System.out.println("Final SQL Query: " + baseQuery);
         return baseQuery.toString();
     }
+
+
+    private void fromAndToSelectedDate(String status, String fromDate, String toDate, String selectedDate, StringBuilder baseQuery) {
+        if (status.equalsIgnoreCase("issued")) {
+            if (!fromDate.isEmpty() && !toDate.isEmpty()) {
+                baseQuery.append("AND DATE(cs.updated_date) BETWEEN '").append(fromDate).append("' AND '").append(toDate).append("' ");
+            }
+            if (!selectedDate.isEmpty()) {
+                baseQuery.append("AND DATE(cs.updated_date) = '").append(selectedDate).append("' ");
+            }
+        }
+        if (status.isEmpty()) {
+            baseQuery.append("AND DATE(cs.updated_date) = '").append(selectedDate).append("' ");
+        }
+    }
+
+
 }
