5a5bdbe657b28297f1984f0d852f31d5e0456bc7
saurabh17kumar <kumar.saurabh@dbalounge.com>
2024-07-31 15:49:32 +0530

    remove old password

--- src/main/java/cheque/handover/services/Controller/Admin.java
+++ src/main/java/cheque/handover/services/Controller/Admin.java
@@ -99,7 +99,7 @@ public class Admin {
             if (userDetailOptional.isPresent()) {
                 service.setUpdatePasswordResponse(updatePassword, userDetailOptional.get(), commonResponse);
                 if (commonResponse.getCode() == null) {
-                    commonResponse = service.updateOldPassword(updatePassword, userDetailOptional.get());
+                    return service.updateOldPassword(updatePassword, userDetailOptional.get());
                 }
             } else {
                 commonResponse.setMsg("user not found");
