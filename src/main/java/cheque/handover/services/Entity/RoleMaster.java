45af75c056c089fb383ec62e3bfde9ea8208edbf
nainish <pmo_dbalounge@shubham.co>
2024-11-05 18:52:26 +0530

    auto to identity

--- src/main/java/cheque/handover/services/Entity/RoleMaster.java
+++ src/main/java/cheque/handover/services/Entity/RoleMaster.java
@@ -9,7 +9,7 @@ import lombok.Data;
 @Table(name = "role_master")
 public class RoleMaster {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "role_id")
     private Long role_id;
     @Column(name = "role")
