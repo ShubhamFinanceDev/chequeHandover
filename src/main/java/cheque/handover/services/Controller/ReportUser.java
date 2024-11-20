e4629a2004734f2f2e3be8918541fa7ee551cb7c
user <user@Email.com>
2024-11-20 19:44:42 +0530

    dual login and multi application report changes

--- src/main/java/cheque/handover/services/Controller/ReportUser.java
+++ src/main/java/cheque/handover/services/Controller/ReportUser.java
@@ -3,6 +3,8 @@ package cheque.handover.services.Controller;
 import cheque.handover.services.ServiceImpl.ReportUserServiceImpl;
 import cheque.handover.services.Services.Service;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,11 +14,13 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/report-user")
 public class ReportUser {
 
+    private static final Logger log = LoggerFactory.getLogger(ReportUser.class);
     @Autowired
     private ReportUserServiceImpl service;
 
     @PostMapping("/excel-sheet-builder")
     public ResponseEntity<?> getDataForNewRole(@RequestParam(value = "applicationNo",required = false)String applicationNo, @RequestParam(value = "file",required = false)MultipartFile file, HttpServletResponse response){
+        log.info("Legality report invoked {} for", file!=null ? "Multi application":applicationNo);
         return ResponseEntity.ok(service.excelExportService(applicationNo, file, response)).getBody();
     }
 }
